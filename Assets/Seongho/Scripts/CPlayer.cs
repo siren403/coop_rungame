0a7e73ec4035a7c6ec6b32b3406bedfaadf697e8
qkrsogusl3 <qkrsogusl3@gmail.com>
2017-04-04 01:02:57 +0900

    Player - 전진이동, 점프

---
+++ Assets/Seongho/Scripts/CPlayer.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CPlayer : MonoBehaviour
+{
+
+    public float JumpPower = 10.0f;
+    public ForceMode JumpForceMode;
+    public float Speed = 100.0f;
+
+    public bool mIsRun = false;
+
+    private Rigidbody mBody = null;
+    public Rigidbody Body
+    {
+        get
+        {
+            if(mBody == null)
+            {
+                mBody = GetComponent<Rigidbody>();
+            }
+            return mBody;
+        }
+    }
+
+    public bool IsGround = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            mIsRun = true;
+        }
+
+        DoMove();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DoJump();
+        }
+       
+    }
+
+    private void DoJump()
+    {
+        if (IsGround)
+        {
+            IsGround = false;
+            GetComponent<Rigidbody>().AddForce(0, JumpPower, 0, JumpForceMode);
+        }
+    }
+    private void DoMove()
+    {
+        if (mIsRun)
+        {
+            Vector3 pos = this.transform.position;
+            pos.z += Speed * Time.deltaTime;
+            Body.MovePosition(pos);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.collider.CompareTag("tagGround"))
+        {
+            IsGround = true;
+        }
+    }
+
+}
---
+++ Assets/Seongho/Scripts/CTargetCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CTargetCamera : MonoBehaviour
+{
+    public GameObject Target = null;
+
+    public Vector3 Offset = Vector3.zero;
+    public Vector3 TargetOffset = Vector3.zero;
+
+
+    private void LateUpdate()
+    {
+        if(Target == null)
+        {
+            return;
+        }
+
+        UpdatePosition();
+    }
+
+    [ContextMenu("Reposition")]
+    public void UpdatePosition()
+    {
+        Vector3 pos = Target.transform.position;
+        pos += Offset;
+        this.transform.position = pos;
+
+        Vector3 targetPos = Target.transform.position;
+        targetPos += TargetOffset;
+        this.transform.LookAt(targetPos);
+    }
+
+}
