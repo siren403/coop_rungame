01acc0f9de466ef91e8c47f3d5c7ee0cd7f27fa1
SimPlePig <rlatjrfo2@naver.com>
2017-04-06 14:51:39 +0900

    슬라이드버튼 이동함수구현& 아이템사용 버튼 추상화

--- Assets/SeokRae/1_Scripts/CScenePlayGame.cs
+++ Assets/SeokRae/1_Scripts/CScenePlayGame.cs
@@ -10,19 +10,32 @@ public class CScenePlayGame : MonoBehaviour {
     public CUIPlayGame mUIGame;
     public GameObject mCube;
 
-	// Use this for initialization
-	void Start () {
+    public float mPlayerSpeed = 0.0f;
+
+    // Use this for initialization
+    void Start () {
         
 	}
 	
 	// Update is called once per frame
 	void Update () {
         JumpAndSlid();
-        JoyStickMove();
+        //JoyStickMove();
+
+        Debug.Log(mUIGame.GetJoystickDirection());
+
+        Vector3 pos = mCube.transform.position;
+        pos.x += mUIGame.GetJoystickDirection() * 10 * Time.deltaTime;
+        mCube.transform.position = pos;
 
         mUIGame.SetOnSliding(OnSliding);
         mUIGame.SetOnJumping(OnJump);
+        mUIGame.SetOnItemBtn_1(OnUseItem_1);
+        mUIGame.SetOnItemBtn_2(OnUseItem_2);
+
     }
+
+
     void JumpAndSlid()
     {
         Vector3 JumpAndSlidPos = mCube.transform.position;
@@ -30,15 +43,14 @@ public class CScenePlayGame : MonoBehaviour {
         mCube.transform.position = JumpAndSlidPos;
 
     }
+    /*
     void JoyStickMove()
     {
         Vector3 JoyStickPos = mCube.transform.position;
         JoyStickPos.x = mUIGame.GetJoyStickDirection();
         mCube.transform.position = JoyStickPos;
     }
-
-
-
+    */
 
 
 
@@ -54,6 +66,18 @@ public class CScenePlayGame : MonoBehaviour {
         JumpAndSlidDirection = -1;
         CScore.mInst.AddCombo(1);//Combo
     }
+    void OnUseItem_1()
+    {
+        Debug.Log("ItemUse1");
+    }
+    void OnUseItem_2()
+    {
+        Debug.Log("ItemUse2");
+    }
+
+
+
+    
     public float GetJumpAndSlidDirection()
     {
         return JumpAndSlidDirection;
