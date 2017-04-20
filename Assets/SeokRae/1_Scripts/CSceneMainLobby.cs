8acd6d6c92e3bed0d0d5272a8d6236ec2b0338e7
SimPlePig <rlatjrfo2@naver.com>
2017-04-20 10:21:41 +0900

    코인&생명구입창구현

---
+++ Assets/SeokRae/1_Scripts/CSceneMainLobby.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CSceneMainLobby : MonoBehaviour {
+
+
+    public CUIMainLobby mUIMainLobby;
+    
+
+
+    void Start () {
+
+        
+    }
+	
+	void Update () {
+        mUIMainLobby.SetStart(OnStart);
+        mUIMainLobby.SetJump(OnJump);
+        mUIMainLobby.SetSlid(OnSlid);
+        mUIMainLobby.SetItem_1(OnItem_1);
+        mUIMainLobby.SetItem_2(OnItem_2);
+        mUIMainLobby.SetCoinShop(OnCoinShopEnter);
+        mUIMainLobby.SetHeartShop(OnHeartShopEnter);
+        mUIMainLobby.SetBackTitle(OnBackToTitle);
+
+    }
+    
+    void OnStart()
+    {
+        Debug.Log("하트지워지게!!!");
+    }
+
+    void OnJump()
+    {
+        Debug.Log("Jump!");
+    }
+
+    void OnSlid()
+    {
+        Debug.Log("Slid!");
+    }
+
+    void OnItem_1()
+    {
+        Debug.Log("Item1!!");
+    }
+
+    void OnItem_2()
+    {
+        Debug.Log("Item2!!");
+    }
+
+    void OnCoinShopEnter()
+    {
+        SceneManager.LoadScene("CoinShop");
+    }
+
+    void OnHeartShopEnter()
+    {
+        SceneManager.LoadScene("HeartShop");
+    }
+
+    void OnBackToTitle()
+    {
+        SceneManager.LoadScene("MainTitleScene");
+    }
+}
---
+++ Assets/SeokRae/1_Scripts/CUIMainLobby.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CUIMainLobby : MonoBehaviour {
+
+    public delegate void CallBackBtn();
+
+    CallBackBtn OnStarting = null;
+    CallBackBtn OnJumping = null;
+    CallBackBtn OnSliding = null;
+    CallBackBtn OnItem_1 = null;
+    CallBackBtn OnItem_2 = null;
+    CallBackBtn OnCoinShop = null;
+    CallBackBtn OnHeartShop = null;
+    CallBackBtn OnBackToTitle = null;
+    
+
+
+
+    void Start () {
+		
+	}
+
+
+	void Update () {
+		
+	}
+
+
+    public void OnClickStart()
+    {
+        if(null != OnStarting)
+        {
+            OnStarting();
+        }
+       // Debug.Log("???");
+    }
+    public void SetStart(CallBackBtn tA)
+    {
+        OnStarting = tA;
+    }
+
+    public void OnClickJump()
+    {
+        if(null != OnJumping)
+        {
+            OnJumping();
+        }
+    }
+    public void SetJump(CallBackBtn tA)
+    {
+        OnJumping = tA;
+    }
+
+    public void OnClickSlid()
+    {
+        if(null!=OnSliding)
+        {
+            OnSliding();
+        }
+    }
+    public void SetSlid(CallBackBtn tA)
+    {
+        OnSliding = tA;
+    }
+
+    public void OnClickItem_1()
+    {
+        if(null!=OnItem_1)
+        {
+            OnItem_1();
+        }
+    }
+    public void SetItem_1(CallBackBtn tA)
+    {
+        OnItem_1 = tA;
+    }
+
+    public void OnClickItem_2()
+    {
+        if(null!=OnItem_2)
+        {
+            OnItem_2();
+        }
+    }
+    public void SetItem_2(CallBackBtn tA)
+    {
+        OnItem_2 = tA;
+    }
+
+    public void OnClickCoinShop()
+    {
+        if(null!=OnCoinShop)
+        {
+            OnCoinShop();
+        }
+    }
+    public void SetCoinShop(CallBackBtn tA)
+    {
+        OnCoinShop = tA;
+    }
+
+    public void OnClickHeartShop()
+    {
+        if(null!=OnHeartShop)
+        {
+            OnHeartShop();
+        }
+    }
+    public void SetHeartShop(CallBackBtn tA)
+    {
+        OnHeartShop = tA;
+    }
+
+    public void OnClickBackTitle()
+    {
+        if(null!=OnBackToTitle)
+        {
+            OnBackToTitle();
+        }
+    }
+    public void SetBackTitle(CallBackBtn tA)
+    {
+        OnBackToTitle = tA;
+    }
+}
