0acf8991e0b28a79bbc2ede93412b59c6d233a6e
6kigs10-PC\6kigs10 <hanwoozz91@gmail.com>
2017-04-27 20:06:39 +0900

    하트(재화) 저장 할 PlayerPrefsFloat 구현

---
+++ Assets/Seongho/Scripts/Helper/PlayerPrefsFloat.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ 
+
+public class PlayerPrefsFloat
+{
+
+    public float Value
+    {
+        get
+        {
+            return PlayerPrefs.GetFloat(mKey, 0.0f);
+        }
+        set
+        {
+            PlayerPrefs.SetFloat(mKey, value);
+            PlayerPrefs.Save();
+        }
+    }
+    private string mKey = "";
+
+
+    public PlayerPrefsFloat(string key, float value = 0)
+    {
+        mKey = key;
+        if (PlayerPrefs.HasKey(key) == false)
+        {
+            PlayerPrefs.SetFloat(mKey, value);
+        }
+    }
+}
+
+
