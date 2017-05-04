d0d422b536fde6fbb12d141bb3dacdc173b8e27d
qkrsogusl3 <qkrsogusl3@gmail.com>
2017-05-04 15:45:26 +0900

    - 인앱결제 플러그인 임포트

---
+++ Assets/Seongho/Scripts/InApp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Purchasing;
+
+public class InApp : MonoBehaviour
+{
+    private void Awake()
+    {
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
