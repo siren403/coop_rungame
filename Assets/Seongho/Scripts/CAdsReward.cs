97f9d26f34e30d5c0fee25aae496776ea1a970e1
qkrsogusl3 <qkrsogusl3@gmail.com>
2017-05-10 17:12:31 +0900

    - 프리팹 깨짐 현상 복구

--- Assets/Seongho/Scripts/CAdsReward.cs
+++ Assets/Seongho/Scripts/CAdsReward.cs
@@ -15,44 +15,44 @@ public class CAdsReward : MonoBehaviour
 
     public void OnClickBtnShowAds()
     {
-        if (Advertisement.IsReady("rewardedVideo"))
-        {
-            var options = new ShowOptions { resultCallback = HandleShowResult };
-            Advertisement.Show("rewardedVideo", options);
-        }
+        //if (Advertisement.IsReady("rewardedVideo"))
+        //{
+        //    var options = new ShowOptions { resultCallback = HandleShowResult };
+        //    Advertisement.Show("rewardedVideo", options);
+        //}
     }
 
-    private void HandleShowResult(ShowResult result)
-    {
-        switch (result)
-        {
-            case ShowResult.Finished:
-                Debug.Log("The ad was successfully shown.");
-                mTxtAdsResult.text = "The ad was successfully shown.";
-                //
-                // YOUR CODE TO REWARD THE GAMER
-                // Give coins etc.
-                if(AdsFinished != null)
-                {
-                    AdsFinished.Invoke();
-                }
-                break;
-            case ShowResult.Skipped:
-                Debug.Log("The ad was skipped before reaching the end.");
-                mTxtAdsResult.text = "The ad was skipped before reaching the end.";
-                if (AdsSkipped != null)
-                {
-                    AdsSkipped.Invoke();
-                }
-                break;
-            case ShowResult.Failed:
-                Debug.LogError("The ad failed to be shown.");
-                mTxtAdsResult.text = "The ad failed to be shown.";
-                if (AdsFailed != null)
-                {
-                    AdsFailed.Invoke();
-                }
-                break;
-        }
-    }
+    //private void HandleShowResult(ShowResult result)
+    //{
+    //    switch (result)
+    //    {
+    //        case ShowResult.Finished:
+    //            Debug.Log("The ad was successfully shown.");
+    //            mTxtAdsResult.text = "The ad was successfully shown.";
+    //            //
+    //            // YOUR CODE TO REWARD THE GAMER
+    //            // Give coins etc.
+    //            if(AdsFinished != null)
+    //            {
+    //                AdsFinished.Invoke();
+    //            }
+    //            break;
+    //        case ShowResult.Skipped:
+    //            Debug.Log("The ad was skipped before reaching the end.");
+    //            mTxtAdsResult.text = "The ad was skipped before reaching the end.";
+    //            if (AdsSkipped != null)
+    //            {
+    //                AdsSkipped.Invoke();
+    //            }
+    //            break;
+    //        case ShowResult.Failed:
+    //            Debug.LogError("The ad failed to be shown.");
+    //            mTxtAdsResult.text = "The ad failed to be shown.";
+    //            if (AdsFailed != null)
+    //            {
+    //                AdsFailed.Invoke();
+    //            }
+    //            break;
+    //    }
+    //}
 }
