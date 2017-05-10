4606ab7853b0a984d4ca8db6f4da41742cd5f5e9
qkrsogusl3 <qkrsogusl3@gmail.com>
2017-05-10 11:57:09 +0900

    - OutTrack 경계 배치

---
+++ Assets/Seongho/Scripts/CAdsReward.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+using UnityEngine.UI;
+
+public class CAdsReward : MonoBehaviour
+{
+    public Text mTxtAdsResult = null;
+
+
+    public void OnClickBtnShowAds()
+    {
+        if (Advertisement.IsReady("rewardedVideo"))
+        {
+            var options = new ShowOptions { resultCallback = HandleShowResult };
+            Advertisement.Show("rewardedVideo", options);
+        }
+    }
+
+    private void HandleShowResult(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Finished:
+                Debug.Log("The ad was successfully shown.");
+                mTxtAdsResult.text = "The ad was successfully shown.";
+                //
+                // YOUR CODE TO REWARD THE GAMER
+                // Give coins etc.
+                break;
+            case ShowResult.Skipped:
+                Debug.Log("The ad was skipped before reaching the end.");
+                mTxtAdsResult.text = "The ad was skipped before reaching the end.";
+
+                break;
+            case ShowResult.Failed:
+                Debug.LogError("The ad failed to be shown.");
+                mTxtAdsResult.text = "The ad failed to be shown.";
+
+                break;
+        }
+    }
+}
