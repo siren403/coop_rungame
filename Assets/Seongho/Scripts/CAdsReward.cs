1a71328af1e8e592c9a9a9264f7dad414f7bedc4
qkrsogusl3 <qkrsogusl3@gmail.com>
2017-05-10 16:02:21 +0900

    - 환경 UI 리소스 임포트

--- Assets/Seongho/Scripts/CAdsReward.cs
+++ Assets/Seongho/Scripts/CAdsReward.cs
@@ -40,12 +40,18 @@ public class CAdsReward : MonoBehaviour
             case ShowResult.Skipped:
                 Debug.Log("The ad was skipped before reaching the end.");
                 mTxtAdsResult.text = "The ad was skipped before reaching the end.";
-
+                if (AdsSkipped != null)
+                {
+                    AdsSkipped.Invoke();
+                }
                 break;
             case ShowResult.Failed:
                 Debug.LogError("The ad failed to be shown.");
                 mTxtAdsResult.text = "The ad failed to be shown.";
-
+                if (AdsFailed != null)
+                {
+                    AdsFailed.Invoke();
+                }
                 break;
         }
     }
